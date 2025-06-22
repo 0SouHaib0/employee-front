c7dfb9493ea8dc920d81fae1c1a1d1c8d3a7644f
souhaib <selbahrielmdaghri@ribatis.com>
2025-06-22 23:04:53 +0100

    initial commit employee front with tailwind and antDesign setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  important: true,
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
+  theme: {
+    fontFamily:{
+      inter: ['Inter', 'sans-serif'],
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
