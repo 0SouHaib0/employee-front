6e1eeaba275eed65c5738af4064a34204b05a2ad
souhaib <selbahrielmdaghri@ribatis.com>
2025-06-23 19:22:07 +0100

    search api

--- src/api/employeeApi.js
+++ src/api/employeeApi.js
@@ -6,9 +6,13 @@ const BASE_URL = 'http://localhost:8080/api/employee';
 export const getAllEmployees = () => axios.get(BASE_URL);
 
 export const getEmployeeById = (id) => axios.get(`${BASE_URL}/${id}`);
+ 
+export const searchEmployees = (query) => {
+  return axios.get(`${BASE_URL}/search?query=${encodeURIComponent(query)}`);
+};
 
 export const createEmployee = (employee) => axios.post(BASE_URL, employee);
-
+ 
 export const updateEmployee = (id, employee) => axios.put(`${BASE_URL}/${id}`, employee);
 
 export const deleteEmployee = (id) => axios.delete(`${BASE_URL}/${id}`);
